649ec5eba8dc571bd2bbff606e0eb2145c4a9782
Rabgyen <rabmoktan10@gmail.com>
2026-07-12 20:26:35 +0545

    Changed repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/College-Hackathon-and-CodeFest/",
+  base: "/hackathon/",
   plugins: [react(),
      tailwindcss(),
   ],
